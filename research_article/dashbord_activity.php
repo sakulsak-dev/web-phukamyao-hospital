fb0cb8c2b44f7e51f47c229fb7942f33769a42e3
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 15:02:00 +0700

    Add files via upload

---
+++ research_article/dashbord_activity.php
@@ -0,0 +1,226 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="โรงพยาบาลภูกามยาว จังหวัดพะเยา 177 หมู่ 2 ตำบลห้วยแก้ว อำเภอภูกามยาว จังหวัดพะเยา 56000 ">
+    <meta name="keywords" content="โรงพยาบาลภูกามยาว จังหวัดพะเยา 177 หมู่ 2  ตำบลห้วยแก้ว อำเภอภูกามยาว จังหวัดพะเยา 56000
+    © 2022 Phukamyao Hospital. All Rights Reserved. IT Phukamyao Hospital">
+    <link rel="shortcut icon" href="../dist/img/icon-maetha.png">
+    <title>โรงพยาบาลภูกามยาว | Phukamyao Hospital : เกี่ยวกับเรา</title>
+    <link rel="stylesheet" href="../dist/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../dist/css/scrolltop.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/submenu.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/slide.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/carousel.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/font-awesome.min.css" type="text/css" />
+
+    <script type="text/javascript" src="../dist/js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="../dist/js/jssor.slider.mini.js"></script>
+    <script type="text/javascript" src="../dist/js/scrolltop.js"></script>
+    <script type="text/javascript" src="../ist/js/slide.js"></script>
+
+    <style>
+    .affix{
+      top:0;
+      width:100%;
+      z-index:9999 !important;
+    }
+    .affix + .container-fluid{
+      padding-top:70px;
+    }
+    </style>
+        <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcsjR4mAAAAAOY8HN2jwM3HXRrP8hAxgvZqj-Bp"></script>
+        <!-- <script>
+        grecaptcha.enterprise.ready(function() {
+            grecaptcha.enterprise.execute('6LcsjR4mAAAAAOY8HN2jwM3HXRrP8hAxgvZqj-Bp', {action: 'login'}).then(function(token) {
+            ...
+            });
+        });
+        </script> -->
+  </head>
+  <body id="top">
+    <!-- navbar -->
+      <?php
+        include("navbar.php");
+      ?>
+    <!-- end navbar -->
+    
+    
+    <div class="container">
+      <div class="row">
+        <div class='col-md-12' align='center' style="padding-top:40px;">
+          <h1><span class="fa fa-lightbulb-o"></span>&nbsp;บทความ และสาระน่ารู้</h1>
+          <p style='font-size:16px; font-weight:bold;'>บทความ สาระน่ารู้ เรื่องต่างๆ รวบรวมไว้มากมายเพื่อเป็นประโชยน์แก่ผู้เข้ามา</p>
+        </div>
+      </div>
+      <div class='row'>
+      <div class='col-md-12'><a href='article_list.php?article_type_id=1'><h3 class='page-header'>สาระน่ารู้จากโรงพยาบาล</h3></a></div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=20'><img src='img_article/1650956461.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>10 คุณประโยชน์ของน้ำเปล่า</h4>
+                    <span>การดื่มน้ำในปริมาณที่เหมาะสม ทำให้ร่างกายรู้สึกสดชื่น ช่วยให้อวัยวะต่า...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 09 ธ.ค. 2565</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16461</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=19'><img src='img_article/1529978010.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>กิน "ไข่" กันเถอะ</h4>
+                    <span>รู้หรือไม่ ...ไข่มีคุณค่าทางโภชนาการสูง ไข่มีเลซิติน โอลีน วิตามินบี12...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 26 มิ.ย. 2561</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 24282</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=18'><img src='img_article/1521514479.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>หยุดเสี่ยงด้วยคาถา 5ย. ป้องกัน...</h4>
+                    <span>&ldquo;เป็นแล้วตายสถานเดียว&rdquo; ถามว่าน่ากลัวมั้ยสำหรับ &ldquo;โรคพ...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 20 มี.ค. 2561</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16216</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=16'><img src='img_article/1510714150.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>วิธีทำ CPR ที่ถูกต้อง ช่วยให้ร...</h4>
+                    <span>มาดูวิธีทำ CPR ที่ถูกต้อง เผื่อเราเจอสถานการณ์คับขัน มีโอกาสได้ช่วยชีว...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 15 พ.ย. 2560</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16224</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-12'><a href='article_list.php?article_type_id=1'><p class='btn btn-default btn-block' style='margin-top:10px;'>อ่านทั้งหมด</p></a></div><div class='col-md-12'><a href='article_list.php?article_type_id=3'><h3 class='page-header'>รักสุขภาพ</h3></a></div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=17'><img src='img_article/1519698980.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>วิ่งแล้วขาใหญ่ จริงไหม?</h4>
+                    <span>วิ่งแล้วน่องใหญ่ขึ้น&hellip;นี่คือสิ่งที่ผู้หญิงเรากลัวนักหนาว่าการวิ่...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 27 ก.พ. 2561</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16238</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=8'><img src='img_article/1503130047.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>5 วิธีออกกำลังกายเพื่อลดน้ำหนั...</h4>
+                    <span>5&nbsp;วิธีออกกำลังกายเพื่อลดน้ำหนักผิดๆ ที่คนมักไม่รู้ตัว
+&nbsp;
+1....</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 07 ต.ค. 2560</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16211</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=5'><img src='img_article/1503130420.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>งานวิจัยชี้…วิ่ง 1ชั่วโมง อายุ...</h4>
+                    <span>&nbsp; &nbsp;ช่วงที่ผ่านมานี้มีงานวิจัยเกี่ยวกับการวิ่งออกมามากมายครับ...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 07 ต.ค. 2560</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16212</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=3'><img src='img_article/1503130328.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>เทคนิค “วิ่งลดพุง”</h4>
+                    <span>เพื่อนๆทุกคนคงไม่มีใครอยากมีพุงใช้ไหมครับ:) นอกจากจะลำบากในการหาเสื้อผ...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 07 ต.ค. 2560</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16212</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-12'><a href='article_list.php?article_type_id=3'><p class='btn btn-default btn-block' style='margin-top:10px;'>อ่านทั้งหมด</p></a></div><div class='col-md-12'><a href='article_list.php?article_type_id=5'><h3 class='page-header'>HOSxP</h3></a></div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=12'><img src='img_article/1506692396.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>สรุปปัญหาจากการบันทึกข้อมูล 43...</h4>
+                    <span>
+
+
+ลำดับที่
+ชื่อแฟ้ม
+ผู้รับผิดชอบ
+สาเหตุที่ตรวจสอบไม่ผ่าน
+วิธีก...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 29 ก.ย. 2560</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16217</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=2'><img src='img_article/1503129957.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>ระบบงานศูนย์ตรวจสอบสิทธิ</h4>
+                    <span>1.&nbsp; ข้อมูลพื้นฐาน
+1.1&nbsp; สามารถกำหนดข้อมูลสิทธิการรักษาโดยเชื...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 19 ส.ค. 2560</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16211</span>
+                    </a>
+                  </div>
+                </div>
+              </div><div class='col-md-3'>
+                <div class='thumbnail'>
+                  <a href='article_detail.php?id=1'><img src='img_article/1503129948.jpg' data-holder-rendered='true' class='img-responsive'>
+                  <div class='caption'>
+                    <h4>ระบบงานห้องเวชระเบียน</h4>
+                    <span>1. การบันทึกข้อมูลประวัติผู้ป่วย
+1.1&nbsp;&nbsp; สามารถบันทึกข้อมูลปร...</span>
+                    <br>
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 19 ส.ค. 2560</sapn> 
+                          <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 16211</span>
+                    </a>
+                  </div>
+                </div>
+              </div>      </div>
+    </div><!-- /.container -->
+    <!-- FOOTER -->
+             <!-- credit -->
+             <?php
+        include('../credit/credit.php');
+    ?>
+
+    <!-- Footer -->
+    <?php
+        include('../footer.php');
+    ?>
+
+<div class="tab"></div>
+
+    <script src="../dist/js/bootstrap.min.js"></script>
+
+    <!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+    <script src="js/holder.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
---
+++ research_article/research_article.php
@@ -0,0 +1,167 @@
+<div class="row">
+        <div class='col-md-6'>
+            <a href="article.php">
+                <h3 class="page-header">บทความสาระน่ารู้ล่าสุด</h3>
+            </a>
+            <div class='clearfix visible-xs'>
+                <div class='thumbnail'>
+                    <a href='article_detail.php?id=20'>
+                        <img src='img_article/thumb700_1650956461.jpg' data-holder-rendered='true'
+                            class='img-responsive' alt='' title=''>
+                        <div class='caption'>
+                            <h4>10 คุณประโยชน์ของน้ำเปล่า</h4>
+                            <span>การดื่มน้ำในปริมาณที่เหมาะสม ทำให้ร่างกายรู้สึกสดชื่น ช่วยให้อวัยวะต่าง ๆ ท...</span>
+                    </a>
+                    <br>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-calendar'
+                            style='color:#5a5a5a'></span> 09 ธ.ค. 2565</sapn>
+                        <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open'
+                                style='color:#5a5a5a'></span> 16276</span>
+                </div>
+   
+            </div>
+        </div>
+        <div class='clearfix hidden-xs media'>
+            <div class='media-left'>
+                <a href='article_detail.php?id=20'>
+                    <img class='media-object' src='img_article/thumb700_1650956461.jpg' alt='10 คุณประโยชน์ของน้ำเปล่า'
+                        title='' width='190' height='101'>
+                </a>
+            </div>
+            <div class='media-body'>
+                <a href='article_detail.php?id=20'>
+                    <h4>10 คุณประโยชน์ของน้ำเปล่า</h4>
+                    <span>การดื่มน้ำในปริมาณที่เหมาะสม ทำให้ร่างกายรู้สึกสดชื่น ช่วยให้อวัยวะต่าง ๆ
+                        ทำงานได้อย่างเต็มประสิทธิภาพ แต่น้ำเปล่ามีประโยชน์มากกว่าที่คิด ลองมาดูกันดีกว่าว่ามีอะไรบ้าง
+                        1...</span>
+                </a>
+                <br>
+                <span style='font-size:12px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 09
+                    ธ.ค. 2565</sapn>
+                    <span style='font-size:12px'><span class='glyphicon glyphicon-eye-open'
+                            style='color:#5a5a5a'></span> 16276</span>
+
+            </div>
+        </div>
+        <div class='clearfix visible-xs'>
+            <div class='thumbnail'>
+                <a href='article_detail.php?id=19'>
+                    <img src='img_article/thumb700_1529978010.jpg' data-holder-rendered='true' class='img-responsive'
+                        alt='' title=''>
+                    <div class='caption'>
+                        <h4>กิน "ไข่" กันเถอะ</h4>
+                        <span>รู้หรือไม่ ...ไข่มีคุณค่าทางโภชนาการสูง ไข่มีเลซิติน โอลีน วิตามินบี12 ซึ่ง...</span>
+                </a>
+                <br>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 26
+                    มิ.ย. 2561</sapn>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open'
+                            style='color:#5a5a5a'></span> 24279</span>
+            </div>
+
+        </div>
+    </div>
+
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='article_detail.php?id=19'>
+                <img class='media-object' src='img_article/thumb700_1529978010.jpg' alt='กิน "ไข่" กันเถอะ' title=''
+                    width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='article_detail.php?id=19'>
+                <h4>กิน "ไข่" กันเถอะ</h4>
+                <span>รู้หรือไม่ ...ไข่มีคุณค่าทางโภชนาการสูง ไข่มีเลซิติน โอลีน วิตามินบี12
+                    ซึ่งเป็นสารอาหารที่หาได้ยากจากอาหารอื่น ซึ่งมีโปรตีนอยู่ในไข่มากมาย
+                    แล้วยับไปช่วยสมานแผลได้อีกด้วย&...</span>
+            </a>
+            <br>
+            <span style='font-size:12px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 26
+                มิ.ย. 2561</sapn>
+                <span style='font-size:12px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    24279</span>
+
+        </div>
+    </div>
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='article_detail.php?id=18'>
+                <img src='img_article/thumb700_1521514479.jpg' data-holder-rendered='true' class='img-responsive' alt=''
+                    title=''>
+                <div class='caption'>
+                    <h4>หยุดเสี่ยงด้วยคาถา 5ย. ป้องกันพิษ...</h4>
+                    <span>&ldquo;เป็นแล้วตายสถานเดียว&rdquo; ถามว่าน่ากลัวมั้ยสำหรับ &ldquo;โรคพิษสุน...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 20
+                มี.ค. 2561</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    16213</span>
+        </div>
+
+    </div>
+    </div>
+    
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='article_detail.php?id=18'>
+                <img class='media-object' src='img_article/thumb700_1521514479.jpg'
+                    alt='หยุดเสี่ยงด้วยคาถา 5ย. ป้องกันพิษสุนัขบ้า' title='' width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='article_detail.php?id=18'>
+                <h4>หยุดเสี่ยงด้วยคาถา 5ย. ป้องกันพิษสุนัขบ้า</h4>
+                <span>&ldquo;เป็นแล้วตายสถานเดียว&rdquo; ถามว่าน่ากลัวมั้ยสำหรับ &ldquo;โรคพิษสุนัขบ้า&rdquo; แน่นอนว่า
+                    ภาพของคนนอนน้ำลายฟูมปาก แล้วค่อยๆ ชักตาย กลายเป็นภาพสยดสยองที่ไม่มีใครอย...</span>
+            </a>
+            <br>
+            <span style='font-size:12px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 20
+                มี.ค. 2561</sapn>
+                <span style='font-size:12px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    16213</span>
+
+        </div>
+    </div>
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='article_detail.php?id=17'>
+                <img src='img_article/thumb700_1519698980.jpg' data-holder-rendered='true' class='img-responsive' alt=''
+                    title=''>
+                <div class='caption'>
+                    <h4>วิ่งแล้วขาใหญ่ จริงไหม?</h4>
+                    <span>วิ่งแล้วน่องใหญ่ขึ้น&hellip;นี่คือสิ่งที่ผู้หญิงเรากลัวนักหนาว่าการวิ่งออกก...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 27
+                ก.พ. 2561</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    16228</span>
+        </div>
+
+    </div>
+    </div>
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='article_detail.php?id=17'>
+                <img class='media-object' src='img_article/thumb700_1519698980.jpg' alt='วิ่งแล้วขาใหญ่ จริงไหม?'
+                    title='' width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='article_detail.php?id=17'>
+                <h4>วิ่งแล้วขาใหญ่ จริงไหม?</h4>
+                <span>วิ่งแล้วน่องใหญ่ขึ้น&hellip;นี่คือสิ่งที่ผู้หญิงเรากลัวนักหนาว่าการวิ่งออกกำลังกายจะมีขอ&shy;&shy;&shy;งแถมเป็นน่องโต
+                    ปูด ๆ ดูไม่สวยงาม เพราะถ้าน่องโตขึ้นนี่ลดให้ลงยากกว่...</span>
+            </a>
+            <br>
+            <span style='font-size:12px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 27
+                ก.พ. 2561</sapn>
+                <span style='font-size:12px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    16228</span>
+
+        </div>
+    </div><a href='article.php'>
+        <p class='btn btn-success btn-block' style='margin-top:10px;'>ทั้งหมด</p>
+    </a> </div>
