1710e1460c4439ef21d83f4dac3929cc3f181868
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 14:56:42 +0700

    Add files via upload

---
+++ link_other/system_other.php
@@ -0,0 +1,56 @@
+<div class="row">
+        <div class="col-md-12">
+            <h3 class="page-header"><span class="glyphicon glyphicon-link"></span>&nbsp;ลิงค์อื่นๆ</h3>
+        </div>
+        <div class="col-md-2 col-sm-2">
+            <a href="http://www.udo.moph.go.th/wasabi3/index.php" class="thumbnail" style="padding:10px;"
+                target='_blank' title="สำนักงานกระทรวงสาธาณสุข จังหวัดพะเยา">
+                <img src='img/link-ssj.jpg' class='img-responsive' alt="สำนักงานกระทรวงสาธาณสุข จังหวัดอุดรธานี"
+                    title="สำนักงานกระทรวงสาธาณสุข จังหวัดอุดรธานี">
+            </a>
+        </div>
+        <div class="col-md-2 col-sm-2" align="center">
+            <a href="http://49.231.241.4/eletter/index.php" class="thumbnail" style="padding:10px;" target='_blank'
+                title="E Letter ระบบส่งเอกสาร">
+                <img src='img/link-eletter.jpg' class='img-responsive' alt="E Letter ระบบส่งเอกสาร">
+            </a>
+        </div>
+        <div class="col-md-2 col-sm-2">
+            <a href="http://hdcservice.moph.go.th/hdc/main/index.php" class="thumbnail" style="padding:10px;"
+                target='_blank' title="HDC กระทรวง">
+                <img src='img/link-hdc1.jpg' class='img-responsive' alt="HDC กระทรวง">
+            </a>
+        </div>
+        <div class="col-md-2 col-sm-2">
+            <a href="https://pyo.hdc.moph.go.th/hdc/main/index.php" class="thumbnail" style="padding:10px;" target='_blank'
+                title="HDC จังหวัด ">
+                <img src='img/link-hdc2.jpg' class='img-responsive' alt="HDC จังหวัด">
+            </a>
+        </div>
+
+        <div class="col-md-2 col-sm-2">
+            <a href="http://203.157.168.24/ltc/index.php?f=ltc2" class="thumbnail" style="padding:10px;" target='_blank'
+                title="LongTermCare ">
+                <img src='img/link-ltc.jpg' class='img-responsive' alt="LongTermCare">
+            </a>
+        </div>
+        <div class="col-md-2 col-sm-2">
+            <a href="http://www.vision2020thailand.org/home.php" class="thumbnail" style="padding:10px;" target='_blank'
+                title="cockpit60 ">
+                <img src='img/link-vision.jpg' class='img-responsive' alt="vision2020thailand">
+            </a>
+        </div>
+        <div class="col-md-2 col-sm-2">
+            <a href="https://www.udo.moph.go.th/udpho_meeting/" class="thumbnail" style="padding:10px;" target='_blank'
+                title="WebConference ">
+                <img src='img/link-webconference.jpg' class='img-responsive' alt="WebConference">
+            </a>
+        </div>
+        <div class="col-md-2 col-sm-2">
+            <a href="http://www.stopcorruption.moph.go.th/" class="thumbnail" style="padding:10px;" target='_blank'
+                title="ศูนย์ปฏิบัติการต่อต้านการทุจริต (ศปท.) กระทรวงสาธารณสุข ">
+                <img src='img/link-acoc.jpg' class='img-responsive'
+                    alt="ศูนย์ปฏิบัติการต่อต้านการทุจริต (ศปท.) กระทรวงสาธารณสุข">
+            </a>
+        </div>
+    </div>
