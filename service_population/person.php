58c499a30a9b27b153a408ffbc329fb756e643d2
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 15:03:04 +0700

    Add files via upload

---
+++ service_population/person.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="โรงพยาบาลภูกามยาว จังหวัดพะเยา 177 หมู่ 2 ตำบลห้วยแก้ว อำเภอภูกามยาว จังหวัดพะเยา 56000 ">
+    <meta name="keywords" content="โรงพยาบาลภูกามยาว จังหวัดพะเยา 177 หมู่ 2  ตำบลห้วยแก้ว อำเภอภูกามยาว จังหวัดพะเยา 56000
+    © 2022 Phukamyao Hospital. All Rights Reserved. IT Phukamyao Hospital">
+    <link rel="shortcut icon" href="../dist/img/icon-maetha.png">
+    <title>โรงพยาบาลภูกามยาว | Phukamyao Hospital : เกี่ยวกับเรา</title>
+    <link rel="stylesheet" href="../dist/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../dist/css/scrolltop.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/submenu.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/slide.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/carousel.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/font-awesome.min.css" type="text/css" />
+
+    <script type="text/javascript" src="../dist/js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="../dist/js/jssor.slider.mini.js"></script>
+    <script type="text/javascript" src="../dist/js/scrolltop.js"></script>
+    <script type="text/javascript" src="../ist/js/slide.js"></script>
+
+  <style type="text/css">
+      .wsm{
+        border-radius:100px; width:100px; line-height: 110px; height: 100px; background-color:#056839;border: 2px solid #056839; display: inline-block; color:#fff;
+      }
+      .affix{
+      top:0;
+      width:100%;
+      z-index:9999 !important;
+    }
+    .affix + .container-fluid{
+      padding-top:70px;
+    }
+    </style>
+    <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcsjR4mAAAAAOY8HN2jwM3HXRrP8hAxgvZqj-Bp"></script>
+
+  </head>
+  <body id="top">
+    <!-- navbar -->
+        <?php
+            include("navbar.php");
+        ?>
+    <!-- end navbar -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12" >
+            <ol class="breadcrumb" style="margin-top:45px;">
+                <li><a href="index.php">หน้าหลัก</a></li>
+                <li class="active" style="color:#056839; font-weight:bold;">บริการประชาชน</li>
+            </ol>
+          </div>
+        
+
+      <div class='col-md-12' align='center' style="padding-top:40px;">
+          <h1><span class="fa fa-smile-o"></span>&nbsp;บริการประชาชน</h1>
+          <p style='font-size:16px; font-weight:bold;'>พื้นที่แนะนำข้อมูล สิทธิการรักษา หรือร้องเรียนเรื่องต่างๆ สำหรับประชาชน</p>
+        </div>
+              
+                <div class="col-md-12" style="margin-top:50px;">
+
+                    <a href="clinic.php?view=1">
+                    <div class='col-md-4' style='text-align: center;'>
+                      <div class="wsm">
+                      <i class='fa fa-plus-square' aria-hidden='true'></i>
+                      </div>
+                      <h3>บริการคลินิกพิเศษ</h3>
+                      <p style='font-weight:bold;'>ข้อมูลวันให้บริการคลินิกพิเศษ</p>
+                   </div>
+                   </a>
+                  <a href="introduce.php?view=5">
+                  <div class='col-md-4' style='text-align: center;'>
+                      <div class="wsm">
+                      <i class='fa fa-id-card-o' aria-hidden='true'></i>
+                      </div>
+                      <h3>สิทธิหลักประกันสุขภาพ</h3>
+                      <p style='font-weight:bold;'>ข้อมูลการใช้สิทธิการรักษา</p>
+                  </div>
+                  </a>
+
+                <a href="http://wwwback.nhso.go.th/peoplesearch" target="_blank">
+                  <div class='col-md-4' style='text-align: center;'>
+                      <div class="wsm">
+                      <i class='fa fa-search' aria-hidden='true'></i>
+                      </div>
+                      <h3>ตรวจสอบสิทธิ</h3>
+                      <p style='font-weight:bold;'>ตรวจสอบสิทธิด้วยตนเอง</p>
+                  </div>
+                  </a>
+
+                  <!--<a href="appeal.php">
+                  <div class='col-md-3 col-sm-6' style='text-align: center;'>
+                      <div class="wsm">
+                      <i class='glyphicon glyphicon-envelope' aria-hidden='true'></i>
+                      </div>
+                      <h3>ร้องเรียนถึง ผอ.</h3>
+                      <p style='font-weight:bold;'>ร้องเรียนการให้บริการ</p>
+                  </div>
+                  </a>-->
+                  <a href="mybody.php?view=1">
+                  <div class='col-md-6' style='text-align: center;'>
+                        <div class="wsm">
+                        <i class='fa fa-calculator' aria-hidden='true'></i>
+                        </div>
+                        <h3>สูตรคำนวณร่างกาย</h3>
+                        <p style='font-weight:bold;'>สำหรับประชาชนทั่วไปและบุคลากร รพ.</p>
+                    </div> 
+                    </a> 
+                <a href="https://www.moph.go.th/index.php/home/app_moph" target="_blank">
+                <div class='col-md-6' style='text-align: center;'>
+                      <div class="wsm">
+                      <i class='fa fa-mobile' aria-hidden='true'></i>
+                      </div>
+                      <h3>Application</h3>
+                      <p style='font-weight:bold;'>กระทรวงสาธารณสุข (สำหรับประชาชนทั่วไป)</p>
+                  </div> 
+                  </a>
+
+                
+                  
+                </div>
+              </div><!-- /.row -->
+      </div><!-- /.container -->
+
+     <!-- credit -->
+    <?php
+        include('../credit/credit.php');
+    ?>
+
+    <!-- Footer -->
+    <?php
+        include('../footer.php');
+    ?>
+
+<div class="tab"></div>   
+<script src="../dist/js/bootstrap.min.js"></script>
+  </body>
+</html>
