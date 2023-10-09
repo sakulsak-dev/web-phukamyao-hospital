a5fa245c890108f3be923ad7ebef36c44abdd465
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 14:38:02 +0700

    Add files via upload

---
+++ contact/contact.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="โรงพยาบาลภูกามยาว จังหวัดพะเยา 177 หมู่ 2 ตำบลห้วยแก้ว อำเภอภูกามยาว จังหวัดพะเยา 56000 ">
+    <meta name="keywords" content="โรงพยาบาลภูกามยาว จังหวัดพะเยา 177 หมู่ 2  ตำบลห้วยแก้ว อำเภอภูกามยาว จังหวัดพะเยา 56000
+    © 2022 Phukamyao Hospital. All Rights Reserved. IT Phukamyao Hospital">
+    <link rel="shortcut icon" href="../dist/img/icon-maetha.png">
+    <title>โรงพยาบาลภูกามยาว | Phukamyao Hospital : เกี่ยวกับเรา</title>
+    <link rel="stylesheet" href="../dist/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../dist/css/scrolltop.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/submenu.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/slide.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/carousel.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/font-awesome.min.css" type="text/css" />
+
+    <script type="text/javascript" src="../dist/js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="../dist/js/jssor.slider.mini.js"></script>
+    <script type="text/javascript" src="../dist/js/scrolltop.js"></script>
+    <script type="text/javascript" src="../ist/js/slide.js"></script>
+
+
+    <style>
+        .affix {
+            top: 0;
+            width: 100%;
+            z-index: 9999 !important;
+        }
+
+        .affix+.container-fluid {
+            padding-top: 70px;
+        }
+    </style>
+     <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcsjR4mAAAAAOY8HN2jwM3HXRrP8hAxgvZqj-Bp"></script>
+        <!-- <script>
+        grecaptcha.enterprise.ready(function() {
+            grecaptcha.enterprise.execute('6LcsjR4mAAAAAOY8HN2jwM3HXRrP8hAxgvZqj-Bp', {action: 'login'}).then(function(token) {
+            ...
+            });
+        });
+        </script> -->
+    <div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v3.0&appId=487744121277814&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+  </head>
+  <body id="top">
+        <!-- navbar -->
+        <?php
+            include("navbar.php");
+        ?>
+    <!--  -->
+    <div class="container">
+      <div class="row">
+      <div class="col-md-4">
+                <h3 class="page-header"><span class="glyphicon glyphicon-earphone"></span>&nbsp;ติดต่อเรา</h3>
+                <h4>โรงพยาบาลภูกามยาว จังหวัดพะเยา</h4> 
+                <p><span class="glyphicon glyphicon-map-marker"></span> ที่อยู่ : เลขที่ 177 หมู่2 ต.ห้วยแก้ว อ.ภูกามยาว จ.พะเยา 56000</p>
+                <p><span class="glyphicon glyphicon-user"></span> ผู้อำนวยการ : .......</p>
+                <p><span class="glyphicon glyphicon-earphone"></span> โทรศัพท์ : 054 079 801</p>
+                <p><span class="glyphicon glyphicon-print"></span> โทรสาร : .........</p>
+                <p><span class="glyphicon glyphicon-envelope"></span> E-mail :phukamyao64@gmail.com </p>
+                <p><span class="glyphicon glyphicon-globe"></span> WebSite : <a href="http://localhost/web-phukamyao-hospital/index.php">http://localhost/web-phukamyao-hospital/index.php</a></p>
+                <div class="fb-page" data-href="https://www.facebook.com/pkyhospital" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/wangsammohospital" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/wangsammohospital">โรงพยาบาลวังสามหมอ</a></blockquote></div>
+            </div>
+          <div class="col-md-8">
+              <h3 class="page-header"><span class="glyphicon glyphicon-map-marker"></span>&nbsp;แผนที่ตั้ง</h3>
+              <div id="map" style="width:100%;height:450px;"></div>
+              <!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAb16lyqI6A2lItwEy1cCzkR8FCuCJ6Uj8&signed_in=true&callback=initMap"></script> -->
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.819271291331!2d99.98124607585682!3d19.29022404521107!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30d82a06ec61577d%3A0xa1d9f07b54da154e!2z4LmC4Lij4LiH4Lie4Lii4Liy4Lia4Liy4Lil4Lig4Li54LiB4Liy4Lih4Lii4Liy4Lin!5e0!3m2!1sth!2sth!4v1696815367297!5m2!1sth!2sth" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+          </div>
+            
+          </div>
+      </div><!-- /.container -->
+
+        <!-- credit -->
+        <?php
+            include('../credit/credit.php');
+        ?>
+
+        <!-- Footer -->
+        <?php
+            include('../footer.php');
+        ?>
+
+    <script src="../dist/js/bootstrap.min.js"></script>
+    <script src="../dist/js/holder.min.js"></script>
+    <script src="../dist/js/ie10-viewport-bug-workaround.js"></script>
+  </body>
+</html>
