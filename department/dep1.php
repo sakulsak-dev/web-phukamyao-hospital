676a600b082fa0bca0b907431b7b607b84f312f3
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 14:40:42 +0700

    Add files via upload

---
+++ department/dep1.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="โรงพยาบาลภูกามยาว จังหวัดพะเยา 177 หมู่ 2 ตำบลห้วยแก้ว อำเภอภูกามยาว จังหวัดพะเยา 56000 ">
+    <meta name="keywords" content="โรงพยาบาลภูกามยาว จังหวัดพะเยา 177 หมู่ 2  ตำบลห้วยแก้ว อำเภอภูกามยาว จังหวัดพะเยา 56000
+    © 2022 Phukamyao Hospital. All Rights Reserved. IT Phukamyao Hospital">
+    <link rel="shortcut icon" href="../dist/img/icon-maetha.png">
+    <title>โรงพยาบาลภูกามยาว | Phukamyao Hospital : เกี่ยวกับเรา</title>
+    <link rel="stylesheet" href="../dist/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="../dist/css/scrolltop.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/submenu.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/slide.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/carousel.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/style.css" type="text/css" />
+    <link rel="stylesheet" href="../dist/css/font-awesome.min.css" type="text/css" />
+
+    <script type="text/javascript" src="../dist/js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="../dist/js/jssor.slider.mini.js"></script>
+    <script type="text/javascript" src="../dist/js/scrolltop.js"></script>
+    <script type="text/javascript" src="../ist/js/slide.js"></script>
+
+
+    <style>
+        .affix {
+            top: 0;
+            width: 100%;
+            z-index: 9999 !important;
+        }
+
+        .affix+.container-fluid {
+            padding-top: 70px;
+        }
+    </style>
+    <!-- <script src="https://www.google.com/recaptcha/enterprise.js?render=6LcsjR4mAAAAAOY8HN2jwM3HXRrP8hAxgvZqj-Bp"></script>
+        <script>
+        grecaptcha.enterprise.ready(function() {
+            grecaptcha.enterprise.execute('6LcsjR4mAAAAAOY8HN2jwM3HXRrP8hAxgvZqj-Bp', {action: 'login'}).then(function(token) {
+            ...
+            });
+        });
+        </script> -->
+</head>
+    <!-- navbar -->
+        <?php
+            include("navbar.php");
+        ?>
+    <!--  -->
+<div class="container marketing">
+
+    <div class="row">
+        <div class='col-md-12' align='center' style="padding-top:40px;">
+            <h1><i class="fa fa-user-md"></i> องค์กรแพทย์</h1>
+            <p style='font-size:16px; font-weight:bold;'></p>
+        </div>
+    </div>
+
+    <div class="row" style="margin-top:40px;text-align: center;">
+        <div class='col-md-12'>
+            <img class='img-circle' src='img_user/user01.jpg' width='200' height='200'>
+            <h3>นายแพทย์กิตติวัฒน์ กันทะ</h3>
+            <p style='font-weight:bold;'>ผู้อำนวยการโรงพยาบาล (นายแพทย์เชี่ยวชาญ)</p>
+        </div>
+    </div><!-- /.row -->
+
+    <div class="row" style="margin-top:30px;text-align: center;">
+        <div class='col-md-12'>
+            <img class='img-circle' src='img_user/dep1/1.jpg' width='160' height='160'>
+            <h3>นพ....</h3>
+            <p style='font-weight:bold;'>นายแพทย์ชำนาญการ</p>
+        </div>
+    </div><!-- /.row -->
+
+    <div class="row" style="margin-top:30px;text-align: center;">
+        <div class='col-md-3 col-sm-6'>
+            <img class='img-circle' src='img_user/dep1/5.jpg' width='140' height='140'>
+            <h3>นพ.....</h3>
+            <p style='font-weight:bold;'>นายแพทย์ชำนาญการ</p>
+        </div>
+        <div class='col-md-3 col-sm-6'>
+            <img class='img-circle' src='img_user/dep1/3.jpg'' width=' 140' height='140'>
+            <h3>นพ.....</h3>
+            <p style='font-weight:bold;'>นายแพทย์ปฏิบัติการ</p>
+        </div>
+        <div class='col-md-3 col-sm-6'>
+            <img class='img-circle' src='img_user/user.jpg'' width=' 140' height='140'>
+            <h3>นพ....</h3>
+            <p style='font-weight:bold;'>นายแพทย์ปฏิบัติการ</p>
+        </div>
+        <div class='col-md-3 col-sm-6'>
+            <img class='img-circle' src='img_user/user.jpg' width='140' height='140'>
+            <h3>พญ.....</h3>
+            <p style='font-weight:bold;'>นายแพทย์ปฏิบัติการ</p>
+        </div>
+    </div><!-- /.row -->
+
+    <div class="row" style="margin-top:30px;text-align: center;">
+        <div class='col-md-4 col-sm-6'>
+            <img class='img-circle' src='img_user/user.jpg' width='140' height='140'>
+            <h3>นพ.....</h3>
+            <p style='font-weight:bold;'>นายแพทย์ปฏิบัติการ</p>
+        </div>
+        <div class='col-md-4 col-sm-6'>
+            <img class='img-circle' src='img_user/user.jpg'' width=' 140' height='140'>
+            <h3>นพ.....</h3>
+            <p style='font-weight:bold;'>นายแพทย์ปฏิบัติการ</p>
+        </div>
+        <div class='col-md-4 col-sm-6'>
+            <img class='img-circle' src='img_user/user.jpg'' width=' 140' height='140'>
+            <h3>นพ.....</h3>
+            <p style='font-weight:bold;'>นายแพทย์ปฏิบัติการ</p>
+        </div>
+
+    </div><!-- /.row -->
+
+    <hr class="featurette">
+</div><!-- /.container -->
+
+    <!-- credit -->
+    <?php
+        include('../credit/credit.php');
+    ?>
+
+    <!-- Footer -->
+    <?php
+        include('../footer.php');
+    ?>
+
+<script src="../dist/js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
