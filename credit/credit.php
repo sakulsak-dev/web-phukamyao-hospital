877a8a4ed60d2f76019451c5370fa0e8618c92fa
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 14:39:37 +0700

    Add files via upload

---
+++ credit/credit.php
@@ -0,0 +1,55 @@
+<div class="credit">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h3><span class="glyphicon glyphicon-globe"></span>&nbsp; เครือข่ายโรงพยาบาล</h3>
+                    <li><a href="https://phukamyao.wordpress.com/" target="_blank">สำนักงานสาธารณสุขอำเภอภูกามยาว</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                    <li><a href="">-</a></li>
+                </div>
+                <div class="col-md-3">
+                    <h3><span class="glyphicon glyphicon-link"></span>&nbsp; หน่วยงานที่เกี่ยวข้อง</h3>
+                    <li><a href="https://www.moph.go.th/" target="_blank">กระทรวงสาธารณสุข</a></li>
+                    <li><a href="http://www.ddc.moph.go.th/" target="_blank">กรมควบคุมโรค</a></li>
+                    <li><a href="https://khonkaen2.nhso.go.th/mis/43F_analysis_prov.php?brn=01&prov=5600" target="_blank">สปสช. เขต1 พะเยา</a></li>
+                    <li><a href="http://webrh1.rh1.go.th/" target="_blank">สำนักงานเขตสุขภาพที่ 1</a></li>
+                    <li><a href="https://pyo.moph.go.th/"
+                            target="_blank">สำนักงานสาธารณสุขจังหวัดพะเยา</a></li>
+
+                </div>
+                <div class="col-md-3">
+                    <h3><span class="fa fa-envelope-o "></span>&nbsp; ข่าวสาร</h3>
+                    <li><a href="public.php">ข่าวประชาสัมพันธ์</a></li>
+                    <li><a href="announce.php">ประกาศ</a></li>
+                    <li><a href="job.php">รับสมัครงาน</a></li>
+                    <li><a href="procure.php">จัดซื้อจัดจ้าง</a></li>
+                    <li><a href="ita.php?y=2566">ITA</a></li>
+                </div>
+                <div class="col-md-3">
+                    <h3><span class="glyphicon glyphicon-hdd"></span>&nbsp; ระบบงานสารสนเทศ</h3>
+                    <li><a href="wsm/" target="_blank">WSM Hospital</a></li>
+                    <!--   <li><a href="labonline/" target="_blank">WSM Lab Online</a></li> -->
+                    <li><a href="medical/" target="_blank">WSM เครื่องมือแพทย์</a></li>
+                    <li><a href="book/user.php" target="_blank">WSM หนังสือราชการ</a></li>
+                    <li><a href="http://wsm.thai-nrls.org/" target="_blank">HRMS ระบบบริหารจัดการความเสี่ยง</a></li>
+                    <li><a href="http://www.cocudn.info/udn/" target="_blank">Smart COC ระบบประสานข้อมูลเยี่ยมบ้าน</a>
+                    </li>
+                    <li><a href="meeting.php">ปฏิทินการจองห้องประชุม</a></li>
+                    <li><a href="download.php">ดาวน์โหลด</a></li>
+                </div>
+                <!--<div class="col-md-3">
+                    <h3 style="color:#5a5a5a;"><span class="glyphicon glyphicon-globe"></span>&nbsp; social network</h3>
+                        <li><a href="#">Facebook</a></li>     
+                </div>-->
+            </div>
+        </div>
+    </div>
