a9544e6a8d0e1cefd94f4df4d324cfee7f826f6a
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 14:57:28 +0700

    Add files via upload

---
+++ notify/notify.php
@@ -0,0 +1,171 @@
+<div class='col-md-6'>
+    <a href="announce.php">
+        <h3 class="page-header">ประกาศ</h3>
+    </a>
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='public_detail.php?id=230'>
+                <img src='img_public/thumb700_1679985217.jpg' data-holder-rendered='true' class='img-responsive' alt='แผนเงินบำรุงประจำปีงบประมาณ 2566' title='แผนเงินบำรุงประจำปีงบประมาณ 2566'>
+                <div class='caption'>
+                    <h4>แผนเงินบำรุงประจำปีงบประมาณ 25...</h4>
+                    <span>แผนการรับ-จ่ายเงินบำรุงโรงพยาบาลวังสามหมอ ประจำปีงบประมาณ 2566</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 28
+                มี.ค. 2566</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span> 5034</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    8</span>
+        </div>
+
+    </div>
+</div>
+<div class='clearfix hidden-xs media'>
+    <div class='media-left'>
+        <a href='public_detail.php?id=230'>
+            <img class='media-object' src='img_public/thumb700_1679985217.jpg' alt='แผนเงินบำรุงประจำปีงบประมาณ 2566' title='แผนเงินบำรุงประจำปีงบประมาณ 2566' width='190' height='101'>
+        </a>
+    </div>
+    <div class='media-body'>
+        <a href='public_detail.php?id=230'>
+            <h4>แผนเงินบำรุงประจำปีงบประมาณ 2566</h4>
+            <span>แผนการรับ-จ่ายเงินบำรุงโรงพยาบาลวังสามหมอ ประจำปีงบประมาณ 2566</span>
+        </a>
+        <br>
+        <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 28
+            มี.ค. 2566</sapn>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                5034</span>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                8</span>
+
+    </div>
+</div>
+<div class='clearfix visible-xs'>
+    <div class='thumbnail'>
+        <a href='public_detail.php?id=231'>
+            <img src='img_public/thumb700_1679985861.jpg' data-holder-rendered='true' class='img-responsive' alt='แนวทางการปฏิบัติตามเกณฑ์จริยธรรมว่าด้วยการจัดซื้อจัดหาและการส่งเสริมการขายยา' title='แนวทางการปฏิบัติตามเกณฑ์จริยธรรมว่าด้วยการจัดซื้อจัดหาและการส่งเสริมการขายยา'>
+            <div class='caption'>
+                <h4>แนวทางการปฏิบัติตามเกณฑ์จริยธร...</h4>
+                <span>แนวทางการปฏิบัติตามเกณฑ์จริยธรรมว่าด้วยการจัดซื้อจัดหาและการส่งเสริมการขายยาและเวชภัณฑ์ที่...</span>
+        </a>
+        <br>
+        <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 28
+            มี.ค. 2566</sapn>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                4866</span>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                4</span>
+    </div>
+
+</div>
+</div>
+<div class='clearfix hidden-xs media'>
+    <div class='media-left'>
+        <a href='public_detail.php?id=231'>
+            <img class='media-object' src='img_public/thumb700_1679985861.jpg' alt='แนวทางการปฏิบัติตามเกณฑ์จริยธรรมว่าด้วยการจัดซื้อจัดหาและการส่งเสริมการขายยา' title='แนวทางการปฏิบัติตามเกณฑ์จริยธรรมว่าด้วยการจัดซื้อจัดหาและการส่งเสริมการขายยา' width='190' height='101'>
+        </a>
+    </div>
+    <div class='media-body'>
+        <a href='public_detail.php?id=231'>
+            <h4>แนวทางการปฏิบัติตามเกณฑ์จริยธรรมว่าด้วยก...</h4>
+            <span>แนวทางการปฏิบัติตามเกณฑ์จริยธรรมว่าด้วยการจัดซื้อจัดหาและการส่งเสริมการขายยาและเวชภัณฑ์ที่มิใช่ยา&nbsp;</span>
+        </a>
+        <br>
+        <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 28
+            มี.ค. 2566</sapn>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                4866</span>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                4</span>
+
+    </div>
+</div>
+<div class='clearfix visible-xs'>
+    <div class='thumbnail'>
+        <a href='public_detail.php?id=225'>
+            <img src='img_public/thumb700_1638246782.jpg' data-holder-rendered='true' class='img-responsive' alt='ขายทอดตลาดทรัพย์สินที่ชำรุด เสื่อมสภาพ จำนวน 162 รายการ' title='ขายทอดตลาดทรัพย์สินที่ชำรุด เสื่อมสภาพ จำนวน 162 รายการ'>
+            <div class='caption'>
+                <h4>ขายทอดตลาดทรัพย์สินที่ชำรุด เส...</h4>
+                <span>ด้วยโรงพยาบาลวังสามหมอจะได้ดำเนินการขายทอดตลาดทรัพย์สินที่ชำรุดเสื่อมภาพ จำนวน 162 รายการ
+                    ...</span>
+        </a>
+        <br>
+        <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 30
+            พ.ย. 2564</sapn>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                4891</span>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                63</span>
+    </div>
+
+</div>
+</div>
+<div class='clearfix hidden-xs media'>
+    <div class='media-left'>
+        <a href='public_detail.php?id=225'>
+            <img class='media-object' src='img_public/thumb700_1638246782.jpg' alt='ขายทอดตลาดทรัพย์สินที่ชำรุด เสื่อมสภาพ จำนวน 162 รายการ' title='ขายทอดตลาดทรัพย์สินที่ชำรุด เสื่อมสภาพ จำนวน 162 รายการ' width='190' height='101'>
+        </a>
+    </div>
+    <div class='media-body'>
+        <a href='public_detail.php?id=225'>
+            <h4>ขายทอดตลาดทรัพย์สินที่ชำรุด เสื่อมสภาพ จ...</h4>
+            <span>ด้วยโรงพยาบาลวังสามหมอจะได้ดำเนินการขายทอดตลาดทรัพย์สินที่ชำรุดเสื่อมภาพ จำนวน 162 รายการ
+                เป็นไปตามระเบียบพัสดุ พ.ศ.2560 วันที่ 22 ธันวาคม 2564 กำหนดข...</span>
+        </a>
+        <br>
+        <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 30
+            พ.ย. 2564</sapn>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                4891</span>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                63</span>
+
+    </div>
+</div>
+
+<div class='clearfix visible-xs'>
+    <div class='thumbnail'>
+        <a href='public_detail.php?id=223'>
+            <img src='img_public/thumb700_1614926316.jpg' data-holder-rendered='true' class='img-responsive' alt='ยกเลืกประกาศรายชื้อผู้ชนะการเสนอราคา' title='ยกเลืกประกาศรายชื้อผู้ชนะการเสนอราคา'>
+            <div class='caption'>
+                <h4>ยกเลืกประกาศรายชื้อผู้ชนะการเส...</h4>
+                <span>ตามที่โรงพยาบาลวังสามหมอ เรื่อง ประกวดราคาอิเล็กทรอนิกส์ (e-bidding)
+                    จ้างปรับปรุงหอพักผู้ป...</span>
+        </a>
+        <br>
+        <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 03
+            มี.ค. 2564</sapn>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                4959</span>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                9</span>
+    </div>
+
+</div>
+</div>
+<div class='clearfix hidden-xs media'>
+    <div class='media-left'>
+        <a href='public_detail.php?id=223'>
+            <img class='media-object' src='img_public/thumb700_1614926316.jpg' alt='ยกเลืกประกาศรายชื้อผู้ชนะการเสนอราคา' title='ยกเลืกประกาศรายชื้อผู้ชนะการเสนอราคา' width='190' height='101'>
+        </a>
+    </div>
+    <div class='media-body'>
+        <a href='public_detail.php?id=223'>
+            <h4>ยกเลืกประกาศรายชื้อผู้ชนะการเสนอราคา</h4>
+            <span>ตามที่โรงพยาบาลวังสามหมอ เรื่อง ประกวดราคาอิเล็กทรอนิกส์ (e-bidding)
+                จ้างปรับปรุงหอพักผู้ป่วยเพื่อรองรับผู้ป่วยติดเชื้อ COVID19 ชนิดมีระบบกรองอากาศ สำ...</span>
+        </a>
+        <br>
+        <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 03
+            มี.ค. 2564</sapn>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                4959</span>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                9</span>
+
+    </div>
+</div><a href='announce.php'>
+    <p class='btn btn-success btn-block' style='margin-top:10px;'>ทั้งหมด</p>
+</a> </div>
+</div>
