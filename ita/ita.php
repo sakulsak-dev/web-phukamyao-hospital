0cac36a827de8af36d8e7c74457c92c867dd2bf7
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 14:55:36 +0700

    Add files via upload

---
+++ ita/ita.php
@@ -0,0 +1,199 @@
+<div class="row">
+        <div class='col-md-12'>
+            <a href="ita.php?y=2566">
+                <h3 class="page-header">ITA การประเมินคุณธรรมและความโปร่งใส่ในการดำเนินงานภาครัฐ</h3>
+            </a>
+            <div class='clearfix visible-xs'>
+                <div class='thumbnail'>
+                    <a href='publicita_detail.php?id=366'>
+                        <img src='img_public_ita/thumb700_public.jpg' data-holder-rendered='true' class='img-responsive'
+                            alt='MOIT22 หน่วยงานมีแนวปฏิบัติที่เคารพสิทธิมนุษยชนและศักดิ์ศรีของผู้ปฏิบัติงาน และรายงานการป้องกันและแก้ไขปัญหาการล่วงละเมิดหรือคุกคามทางเพศในการทำงาน'
+                            title='MOIT22 หน่วยงานมีแนวปฏิบัติที่เคารพสิทธิมนุษยชนและศักดิ์ศรีของผู้ปฏิบัติงาน และรายงานการป้องกันและแก้ไขปัญหาการล่วงละเมิดหรือคุกคามทางเพศในการทำงาน'>
+                        <div class='caption'>
+                            <h4>MOIT22 หน่วยงานมีแนวปฏิบัติที่เคา...</h4>
+                            <span>MOIT22 หน่วยงานมีแนวปฏิบัติที่เคารพสิทธิมนุษยชนและศักดิ์ศรีของผู้ปฏิบัติงาน
+                                และรายงานการป้...</span>
+                    </a>
+                    <br>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-calendar'
+                            style='color:#5a5a5a'></span> 11 ก.ย. 2566</sapn>
+                        <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open'
+                                style='color:#5a5a5a'></span> 24</span>
+                        <span style='font-size:11px'><span class='glyphicon glyphicon-save'
+                                style='color:#5a5a5a'></span> 0</span>
+                </div>
+
+            </div>
+        </div>
+        <div class='clearfix hidden-xs media'>
+            <div class='media-left'>
+                <a href='publicita_detail.php?id=366'>
+                    <img class='media-object' src='img_public_ita/thumb700_public.jpg'
+                        alt='MOIT22 หน่วยงานมีแนวปฏิบัติที่เคารพสิทธิมนุษยชนและศักดิ์ศรีของผู้ปฏิบัติงาน และรายงานการป้องกันและแก้ไขปัญหาการล่วงละเมิดหรือคุกคามทางเพศในการทำงาน'
+                        title='MOIT22 หน่วยงานมีแนวปฏิบัติที่เคารพสิทธิมนุษยชนและศักดิ์ศรีของผู้ปฏิบัติงาน และรายงานการป้องกันและแก้ไขปัญหาการล่วงละเมิดหรือคุกคามทางเพศในการทำงาน'
+                        width='190' height='101'>
+                </a>
+            </div>
+            <div class='media-body'>
+                <a href='publicita_detail.php?id=366'>
+                    <h4>MOIT22 หน่วยงานมีแนวปฏิบัติที่เคารพสิทธิมนุษยชนและศักดิ์ศรีข...</h4>
+                    <span>MOIT22 หน่วยงานมีแนวปฏิบัติที่เคารพสิทธิมนุษยชนและศักดิ์ศรีของผู้ปฏิบัติงาน
+                        และรายงานการป้องกันและแก้ไขปัญหาการล่วงละเมิดหรือคุกคามทางเพศในการทำงาน</span>
+                </a>
+                <br>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                    ก.ย. 2566</sapn>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open'
+                            style='color:#5a5a5a'></span> 24</span>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                        0</span>
+
+            </div>
+        </div>
+        <div class='clearfix visible-xs'>
+            <div class='thumbnail'>
+                <a href='publicita_detail.php?id=365'>
+                    <img src='img_public_ita/thumb700_public.jpg' data-holder-rendered='true' class='img-responsive'
+                        alt='MOIT19 หน่วยงานมีการรายงานผลการส่งเสริมการปฏิบัติตามประมวลจริยธรรมข้าราชการพลเรือน กรณีการเรี่ยไรและกรณีการให้หรือรับของขวัญหรือประโยชน์อื่นใด ประจำปีงบประมาณ พ.ศ. 2566'
+                        title='MOIT19 หน่วยงานมีการรายงานผลการส่งเสริมการปฏิบัติตามประมวลจริยธรรมข้าราชการพลเรือน กรณีการเรี่ยไรและกรณีการให้หรือรับของขวัญหรือประโยชน์อื่นใด ประจำปีงบประมาณ พ.ศ. 2566'>
+                    <div class='caption'>
+                        <h4>MOIT19 หน่วยงานมีการรายงานผลการส่...</h4>
+                        <span>MOIT19 หน่วยงานมีการรายงานผลการส่งเสริมการปฏิบัติตามประมวลจริยธรรมข้าราชการพลเรือน
+                            กรณีการ...</span>
+                </a>
+                <br>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                    ก.ย. 2566</sapn>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open'
+                            style='color:#5a5a5a'></span> 9</span>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                        0</span>
+            </div>
+
+        </div>
+    </div>
+
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='publicita_detail.php?id=365'>
+                <img class='media-object' src='img_public_ita/thumb700_public.jpg'
+                    alt='MOIT19 หน่วยงานมีการรายงานผลการส่งเสริมการปฏิบัติตามประมวลจริยธรรมข้าราชการพลเรือน กรณีการเรี่ยไรและกรณีการให้หรือรับของขวัญหรือประโยชน์อื่นใด ประจำปีงบประมาณ พ.ศ. 2566'
+                    title='MOIT19 หน่วยงานมีการรายงานผลการส่งเสริมการปฏิบัติตามประมวลจริยธรรมข้าราชการพลเรือน กรณีการเรี่ยไรและกรณีการให้หรือรับของขวัญหรือประโยชน์อื่นใด ประจำปีงบประมาณ พ.ศ. 2566'
+                    width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='publicita_detail.php?id=365'>
+                <h4>MOIT19 หน่วยงานมีการรายงานผลการส่งเสริมการปฏิบัติตามประมวลจร...</h4>
+                <span>MOIT19 หน่วยงานมีการรายงานผลการส่งเสริมการปฏิบัติตามประมวลจริยธรรมข้าราชการพลเรือน
+                    กรณีการเรี่ยไรและกรณีการให้หรือรับของขวัญหรือประโยชน์อื่นใด ประจำปีงบประมาณ พ.ศ. 2566</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                ก.ย. 2566</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    9</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    0</span>
+
+        </div>
+    </div>
+
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='publicita_detail.php?id=364'>
+                <img src='img_public_ita/thumb700_public.jpg' data-holder-rendered='true' class='img-responsive'
+                    alt='MOIT17 หน่วยงานมีการประเมินความเสี่ยงการทุจริต ประจำปีงบประมาณ พ.ศ. 2566 อย่างเป็นระบบ'
+                    title='MOIT17 หน่วยงานมีการประเมินความเสี่ยงการทุจริต ประจำปีงบประมาณ พ.ศ. 2566 อย่างเป็นระบบ'>
+                <div class='caption'>
+                    <h4>MOIT17 หน่วยงานมีการประเมินความเส...</h4>
+                    <span>MOIT17 หน่วยงานมีการประเมินความเสี่ยงการทุจริต ประจำปีงบประมาณ พ.ศ. 2566 อย่างเป็นระบบ</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                ก.ย. 2566</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    24</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    0</span>
+        </div>
+
+    </div>
+    </div>
+
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='publicita_detail.php?id=364'>
+                <img class='media-object' src='img_public_ita/thumb700_public.jpg'
+                    alt='MOIT17 หน่วยงานมีการประเมินความเสี่ยงการทุจริต ประจำปีงบประมาณ พ.ศ. 2566 อย่างเป็นระบบ'
+                    title='MOIT17 หน่วยงานมีการประเมินความเสี่ยงการทุจริต ประจำปีงบประมาณ พ.ศ. 2566 อย่างเป็นระบบ'
+                    width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='publicita_detail.php?id=364'>
+                <h4>MOIT17 หน่วยงานมีการประเมินความเสี่ยงการทุจริต ประจำปีงบประม...</h4>
+                <span>MOIT17 หน่วยงานมีการประเมินความเสี่ยงการทุจริต ประจำปีงบประมาณ พ.ศ. 2566 อย่างเป็นระบบ</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                ก.ย. 2566</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    24</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    0</span>
+
+        </div>
+    </div>
+
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='publicita_detail.php?id=363'>
+                <img src='img_public_ita/thumb700_public.jpg' data-holder-rendered='true' class='img-responsive'
+                    alt='MOIT16 หน่วยงานมีรายงานผลการดำเนินงานตามแผนปฏิบัติการป้องกัน ปราบปรามการทุจริต และประพฤติมิชอบ ประจำปีงบประมาณ พ.ศ. 2566 และแผนปฏิบัติการส่งเสริมคุณธรรม ของชมรมจริยธรรมของหน่วยงาน ประจำปีงบประมาณ พ.ศ.'
+                    title='MOIT16 หน่วยงานมีรายงานผลการดำเนินงานตามแผนปฏิบัติการป้องกัน ปราบปรามการทุจริต และประพฤติมิชอบ ประจำปีงบประมาณ พ.ศ. 2566 และแผนปฏิบัติการส่งเสริมคุณธรรม ของชมรมจริยธรรมของหน่วยงาน ประจำปีงบประมาณ พ.ศ.'>
+                <div class='caption'>
+                    <h4>MOIT16 หน่วยงานมีรายงานผลการดำเนิ...</h4>
+                    <span>MOIT16 หน่วยงานมีรายงานผลการดำเนินงานตามแผนปฏิบัติการป้องกัน ปราบปรามการทุจริต
+                        และประพฤติม...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                ก.ย. 2566</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    27</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    0</span>
+        </div>
+
+    </div>
+    </div>
+    
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='publicita_detail.php?id=363'>
+                <img class='media-object' src='img_public_ita/thumb700_public.jpg'
+                    alt='MOIT16 หน่วยงานมีรายงานผลการดำเนินงานตามแผนปฏิบัติการป้องกัน ปราบปรามการทุจริต และประพฤติมิชอบ ประจำปีงบประมาณ พ.ศ. 2566 และแผนปฏิบัติการส่งเสริมคุณธรรม ของชมรมจริยธรรมของหน่วยงาน ประจำปีงบประมาณ พ.ศ.'
+                    title='MOIT16 หน่วยงานมีรายงานผลการดำเนินงานตามแผนปฏิบัติการป้องกัน ปราบปรามการทุจริต และประพฤติมิชอบ ประจำปีงบประมาณ พ.ศ. 2566 และแผนปฏิบัติการส่งเสริมคุณธรรม ของชมรมจริยธรรมของหน่วยงาน ประจำปีงบประมาณ พ.ศ.'
+                    width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='publicita_detail.php?id=363'>
+                <h4>MOIT16 หน่วยงานมีรายงานผลการดำเนินงานตามแผนปฏิบัติการป้องกัน...</h4>
+                <span>MOIT16 หน่วยงานมีรายงานผลการดำเนินงานตามแผนปฏิบัติการป้องกัน ปราบปรามการทุจริต และประพฤติมิชอบ
+                    ประจำปีงบประมาณ พ.ศ. 2566 และแผนปฏิบัติการส่งเสริมคุณธรรม ของชมรมจริยธรรมของหน่วยงาน ...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                ก.ย. 2566</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    27</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    0</span>
+
+        </div>
+    </div><a href='ita.php?y=2566'>
+        <p class='btn btn-success btn-block' style='margin-top:10px;'>ทั้งหมด</p>
+    </a> </div>
+    </div> <!-- end ita -->
