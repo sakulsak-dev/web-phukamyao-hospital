b6c02469681ed5b8dab157103912194273eccfc5
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 15:00:43 +0700

    Add files via upload

---
+++ register_employee/regis.php
@@ -0,0 +1,198 @@
+<div class="row">
+        <div class='col-md-6'>
+            <a href="job.php">
+                <h3 class="page-header">สมัครงาน</h3>
+            </a>
+            <div class='clearfix visible-xs'>
+                <div class='thumbnail'>
+                    <a href='public_detail.php?id=228'>
+                        <img src='img_public/thumb700_1654747342.jpg' data-holder-rendered='true' class='img-responsive'
+                            alt='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างชั่วคราว (รายวัน)'
+                            title='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างชั่วคราว (รายวัน)'>
+                        <div class='caption'>
+                            <h4>รับสมัครบุคลากรเพื่อคัดเลือกบรรจ...</h4>
+                            <span>โรงพยาบาลวังสามหมอ มีความประสงค์ที่จะดำเนินการรับสมัครบุคคล เพื่อคัดเลือกเป...</span>
+                    </a>
+                    <br>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-calendar'
+                            style='color:#5a5a5a'></span> 09 มิ.ย. 2565</sapn>
+                        <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open'
+                                style='color:#5a5a5a'></span> 4931</span>
+                        <span style='font-size:11px'><span class='glyphicon glyphicon-save'
+                                style='color:#5a5a5a'></span> 91</span>
+                </div>
+
+            </div>
+        </div>
+        <div class='clearfix hidden-xs media'>
+            <div class='media-left'>
+                <a href='public_detail.php?id=228'>
+                    <img class='media-object' src='img_public/thumb700_1654747342.jpg'
+                        alt='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างชั่วคราว (รายวัน)'
+                        title='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างชั่วคราว (รายวัน)' width='190' height='101'>
+                </a>
+            </div>
+            <div class='media-body'>
+                <a href='public_detail.php?id=228'>
+                    <h4>รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้...</h4>
+                    <span>โรงพยาบาลวังสามหมอ มีความประสงค์ที่จะดำเนินการรับสมัครบุคคล
+                        เพื่อคัดเลือกเป็นลูกจ้างชั่วคราว(รายวัน) ตำแหน่ง พนักงานช่วยเหลือคนไข้ ประเภทเงินนอกงบประมาณ
+                        จำนวน 4 อัตรา
+                        &n...</span>
+                </a>
+                <br>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 09
+                    มิ.ย. 2565</sapn>
+                    <span style='font-size:11px color:#056839;'><span class='glyphicon glyphicon-eye-open'
+                            style='color:#5a5a5a'></span> 4931</span>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                        91</span>
+
+            </div>
+        </div>
+        <div class='clearfix visible-xs'>
+            <div class='thumbnail'>
+                <a href='public_detail.php?id=227'>
+                    <img src='img_public/thumb700_1650349115.jpg' data-holder-rendered='true' class='img-responsive'
+                        alt='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างชั่วคราว เจ้าพนักงานพัสดุ'
+                        title='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างชั่วคราว เจ้าพนักงานพัสดุ'>
+                    <div class='caption'>
+                        <h4>รับสมัครบุคลากรเพื่อคัดเลือกบรรจ...</h4>
+                        <span>โรงพยาบาลวังสามหมอ มีความประสงค์ที่จะดำเนินการรับสมัครบุคคล เพื่อคัดเลือกเป...</span>
+                </a>
+                <br>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 19
+                    เม.ย. 2565</sapn>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open'
+                            style='color:#5a5a5a'></span> 5017</span>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                        44</span>
+            </div>
+
+        </div>
+    </div>
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='public_detail.php?id=227'>
+                <img class='media-object' src='img_public/thumb700_1650349115.jpg'
+                    alt='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างชั่วคราว เจ้าพนักงานพัสดุ'
+                    title='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างชั่วคราว เจ้าพนักงานพัสดุ' width='190'
+                    height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='public_detail.php?id=227'>
+                <h4>รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้...</h4>
+                <span>โรงพยาบาลวังสามหมอ มีความประสงค์ที่จะดำเนินการรับสมัครบุคคล เพื่อคัดเลือกเป็นลูกจ้างชั่วคราว
+                    ตำแหน่ง&nbsp;เจ้าพนักงานพัสดุ ประเภทเงินนอกงบประมาณ จำนวน 1 อัตรา อัตราค่าจ่า...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 19
+                เม.ย. 2565</sapn>
+                <span style='font-size:11px color:#056839;'><span class='glyphicon glyphicon-eye-open'
+                        style='color:#5a5a5a'></span> 5017</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    44</span>
+
+        </div>
+    </div>
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='public_detail.php?id=215'>
+                <img src='img_public/thumb700_1607422278.jpg' data-holder-rendered='true' class='img-responsive'
+                    alt='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างรายวัน'
+                    title='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างรายวัน'>
+                <div class='caption'>
+                    <h4>รับสมัครบุคลากรเพื่อคัดเลือกบรรจ...</h4>
+                    <span>โรงพยาบาลวังสามหมอ มีความประสงค์ที่จะดำเนินการรับสมัครบุคคล เพื่อคัดเลือกเป...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 08
+                ธ.ค. 2563</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    5062</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    74</span>
+        </div>
+
+    </div>
+    </div>
+
+
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='public_detail.php?id=215'>
+                <img class='media-object' src='img_public/thumb700_1607422278.jpg'
+                    alt='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างรายวัน'
+                    title='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างรายวัน' width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='public_detail.php?id=215'>
+                <h4>รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้...</h4>
+                <span>โรงพยาบาลวังสามหมอ มีความประสงค์ที่จะดำเนินการรับสมัครบุคคล
+                    เพื่อคัดเลือกเป็นลูกจ้างรายวันประเภทเงินนอกงบประมาณ จำนวน 6&nbsp;อัตรา
+                    &nbsp;
+                    ตำแหน่งที่รับสมัคร
+                    - พนักงานช...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 08
+                ธ.ค. 2563</sapn>
+                <span style='font-size:11px color:#056839;'><span class='glyphicon glyphicon-eye-open'
+                        style='color:#5a5a5a'></span> 5062</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    74</span>
+
+        </div>
+    </div>
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='public_detail.php?id=214'>
+                <img src='img_public/thumb700_1595406500.jpg' data-holder-rendered='true' class='img-responsive'
+                    alt='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างรายวัน'
+                    title='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างรายวัน'>
+                <div class='caption'>
+                    <h4>รับสมัครบุคลากรเพื่อคัดเลือกบรรจ...</h4>
+                    <span>โรงพยาบาลวังสามหมอ มีความประสงค์ที่จะดำเนินการรับสมัครบุคคล เพื่อคัดเลือกเป...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 22
+                ก.ค. 2563</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    5001</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    67</span>
+        </div>
+
+    </div>
+    </div>
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='public_detail.php?id=214'>
+                <img class='media-object' src='img_public/thumb700_1595406500.jpg'
+                    alt='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างรายวัน'
+                    title='รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้างรายวัน' width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='public_detail.php?id=214'>
+                <h4>รับสมัครบุคลากรเพื่อคัดเลือกบรรจุเป็นลูกจ้...</h4>
+                <span>โรงพยาบาลวังสามหมอ มีความประสงค์ที่จะดำเนินการรับสมัครบุคคล
+                    เพื่อคัดเลือกเป็นลูกจ้างรายวันประเภทเงินนอกงบประมาณ จำนวน 6&nbsp;อัตรา
+                    &nbsp;
+                    ตำแหน่งที่รับสมัคร
+                    - พนักงานช...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 22
+                ก.ค. 2563</sapn>
+                <span style='font-size:11px color:#056839;'><span class='glyphicon glyphicon-eye-open'
+                        style='color:#5a5a5a'></span> 5001</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    67</span>
+
+        </div>
+    </div><a href='job.php'>
+        <p class='btn btn-success btn-block' style='margin-top:10px;'>ทั้งหมด</p>
+    </a> </div>
