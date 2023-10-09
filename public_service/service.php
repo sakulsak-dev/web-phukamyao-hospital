cf22b7ebc4e9f82d1658509c2039ca5c1e43b935
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 14:59:35 +0700

    Add files via upload

---
+++ public_service/service.php
@@ -0,0 +1,21 @@
+
+<div class="col-md-4">
+        <a href="person.php">
+            <h3 class="page-header">บริการประชาชน</h3>
+        </a>
+        <div class="list-group">
+            <a href="clinic.php?view=1" type="button" class="list-group-item"><span class="fa fa-plus-square"></span>
+                บริการคลินิกพิเศษ</a>
+            <a href="introduce.php?view=5" type="button" class="list-group-item"><span class="fa fa-id-card-o"></span>
+                สิทธิหลักประกันสุขภาพ</a>
+            <!-- <a href="#" type="button" class="list-group-item"><span class="fa fa-heart"></span> ขั้นตอนการมารักษา</a>-->
+            <a href="mybody.php" type="button" class="list-group-item"><span class="fa fa-calculator"></span>
+                สูตรคำนวณร่างกาย</a>
+            <!-- <a href="appeal.php" type="button" class="list-group-item"><span class='glyphicon glyphicon-envelope'></span> ติดต่อร้องเรียน ผอ.</a> -->
+            <a href="https://www.moph.go.th/index.php/home/app_moph" target="_blank" type="button"
+                class="list-group-item"><span class='fa fa-mobile-phone'></span> Application กระทรวงสาธารณสุข</a>
+            <a href="http://wwwback.nhso.go.th/peoplesearch" type="button" class="list-group-item" target="_blank"><span
+                    class="fa fa-search"></span> ตรวจสอบสิทธิด้วยตนเอง</a>
+            <!-- <a href="#" type="button" class="list-group-item">ให้คะแนนแผนกที่ชื่นชอบ</a>-->
+        </div>
+    </div>
