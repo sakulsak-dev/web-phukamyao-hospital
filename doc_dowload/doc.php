558c9cb412985a01e79fcf7b048021d736abcf02
sakulsak-dev <147372578+sakulsak-dev@users.noreply.github.com>
2023-10-09 14:42:10 +0700

    Add files via upload

---
+++ doc_dowload/doc.php
@@ -0,0 +1,209 @@
+<div class="col-md-6">
+        <a href="download.php">
+            <h3 class="page-header">ดาวน์โหลด</h3>
+        </a>
+        <div class='clearfix visible-xs'>
+            <div class='thumbnail'>
+                <a href='public_detail.php?id=50'>
+                    <img src='img_public/thumb700_1513146206.jpg' data-holder-rendered='true' class='img-responsive'
+                        alt='Templates นำเสนอ (ขนาด16:9) ' title='Templates นำเสนอ (ขนาด16:9) '>
+                    <div class='caption'>
+                        <h4>Templates นำเสนอ (ขนาด16:9) </h4>
+                        <span>ไฟล์ Templates นำเสนอ (ขนาด16:9) สี่เหลี่ยมผืนผ้าเต็มจอ โรงพยาบาลวังสามหมอ
+                            ...</span>
+                </a>
+                <br>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 13
+                    ธ.ค. 2560</sapn>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open'
+                            style='color:#5a5a5a'></span> 5301</span>
+                    <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                        275</span>
+            </div>
+
+        </div>
+    </div>
+    
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='public_detail.php?id=50'>
+                <img class='media-object' src='img_public/thumb700_1513146206.jpg' alt='Templates นำเสนอ (ขนาด16:9) '
+                    title='Templates นำเสนอ (ขนาด16:9) ' width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='public_detail.php?id=50'>
+                <h4>Templates นำเสนอ (ขนาด16:9) </h4>
+                <span>ไฟล์ Templates นำเสนอ (ขนาด16:9) สี่เหลี่ยมผืนผ้าเต็มจอ โรงพยาบาลวังสามหมอ
+                    &nbsp;
+                    ตัวอย่าง
+
+                    &nbsp;
+
+                    &nbsp;
+                </span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 13
+                ธ.ค. 2560</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    5301</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    275</span>
+
+        </div>
+    </div>
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='public_detail.php?id=42'>
+                <img src='img_public/thumb700_1507708303.jpg' data-holder-rendered='true' class='img-responsive'
+                    alt='Templates นำเสนอ (4:3)' title='Templates นำเสนอ (4:3)'>
+                <div class='caption'>
+                    <h4>Templates นำเสนอ (4:3)</h4>
+                    <span>ไฟล์&nbsp;Templates นำเสนอ ของโรงพยาบาล
+                        &nbsp;
+                        ตัวอย่าง
+
+                        หน้าแรก
+                        &nbsp...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                ต.ค. 2560</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    5038</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    74</span>
+        </div>
+
+    </div>
+    </div>
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='public_detail.php?id=42'>
+                <img class='media-object' src='img_public/thumb700_1507708303.jpg' alt='Templates นำเสนอ (4:3)'
+                    title='Templates นำเสนอ (4:3)' width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='public_detail.php?id=42'>
+                <h4>Templates นำเสนอ (4:3)</h4>
+                <span>ไฟล์&nbsp;Templates นำเสนอ ของโรงพยาบาล
+                    &nbsp;
+                    ตัวอย่าง
+
+                    หน้าแรก
+                    &nbsp;
+
+                    หน้านำเสนอ
+                    &nbsp;
+
+                    หน้าสุดท้าย</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 11
+                ต.ค. 2560</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    5038</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    74</span>
+
+        </div>
+    </div>
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='public_detail.php?id=38'>
+                <img src='img_public/thumb700_1502769473.jpg' data-holder-rendered='true' class='img-responsive'
+                    alt='แบบฟอร์มรับรองการเกิด ทร 1/1 สำหรับโรงพยาบาลและสถานพยาบาลที่เข้าร่วมโครงการฯ '
+                    title='แบบฟอร์มรับรองการเกิด ทร 1/1 สำหรับโรงพยาบาลและสถานพยาบาลที่เข้าร่วมโครงการฯ '>
+                <div class='caption'>
+                    <h4>แบบฟอร์มรับรองการเกิด ทร 1/1 สำหร...</h4>
+                    <span>แบบฟอร์มรับรองการเกิด ทร 1/1 สำหรับโรงพยาบาลและสถานพยาบาลที่เข้าร่วมโครงการ...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 15
+                ส.ค. 2560</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    5426</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    1074</span>
+        </div>
+
+    </div>
+    </div>
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='public_detail.php?id=38'>
+                <img class='media-object' src='img_public/thumb700_1502769473.jpg'
+                    alt='แบบฟอร์มรับรองการเกิด ทร 1/1 สำหรับโรงพยาบาลและสถานพยาบาลที่เข้าร่วมโครงการฯ '
+                    title='แบบฟอร์มรับรองการเกิด ทร 1/1 สำหรับโรงพยาบาลและสถานพยาบาลที่เข้าร่วมโครงการฯ ' width='190'
+                    height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='public_detail.php?id=38'>
+                <h4>แบบฟอร์มรับรองการเกิด ทร 1/1 สำหรับโรงพยาบาล...</h4>
+                <span>แบบฟอร์มรับรองการเกิด ทร 1/1 สำหรับโรงพยาบาลและสถานพยาบาลที่เข้าร่วมโครงการฯ&nbsp;
+                    &nbsp;
+                    เรียน ผู้เกี่ยวข้องเรื่อง การแจ้งขอ u/p โปรแกรมรับรองการเกิดเจ้าหน้าที่ป...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 15
+                ส.ค. 2560</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    5426</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    1074</span>
+
+        </div>
+    </div>
+    <div class='clearfix visible-xs'>
+        <div class='thumbnail'>
+            <a href='public_detail.php?id=37'>
+                <img src='img_public/thumb700_1502769556.jpg' data-holder-rendered='true' class='img-responsive'
+                    alt='แบบฟอร์มขอ username สปสช.เขต8 อุดรธานี' title='แบบฟอร์มขอ username สปสช.เขต8 อุดรธานี'>
+                <div class='caption'>
+                    <h4>แบบฟอร์มขอ username สปสช.เขต8 อุด...</h4>
+                    <span>โปรดอ่าน!! เงื่อนไขและขั้นตอนการขอ username&nbsp;สปสช.เขต8 อุดรธานี
+                        สิ่งที...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 15
+                ส.ค. 2560</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    4983</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    22</span>
+        </div>
+
+    </div>
+    </div>
+    <div class='clearfix hidden-xs media'>
+        <div class='media-left'>
+            <a href='public_detail.php?id=37'>
+                <img class='media-object' src='img_public/thumb700_1502769556.jpg'
+                    alt='แบบฟอร์มขอ username สปสช.เขต8 อุดรธานี' title='แบบฟอร์มขอ username สปสช.เขต8 อุดรธานี'
+                    width='190' height='101'>
+            </a>
+        </div>
+        <div class='media-body'>
+            <a href='public_detail.php?id=37'>
+                <h4>แบบฟอร์มขอ username สปสช.เขต8 อุดรธานี</h4>
+                <span>โปรดอ่าน!! เงื่อนไขและขั้นตอนการขอ username&nbsp;สปสช.เขต8 อุดรธานี
+                    สิ่งที่ต้องแนบ1. แบบฟอร์มขอ username ลงลายเซนต์หัวหน้างาน หรือผู้บริหาร&nbsp;
+                    &nbsp;&nbsp;&nbsp...</span>
+            </a>
+            <br>
+            <span style='font-size:11px'><span class='glyphicon glyphicon-calendar' style='color:#5a5a5a'></span> 15
+                ส.ค. 2560</sapn>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-eye-open' style='color:#5a5a5a'></span>
+                    4983</span>
+                <span style='font-size:11px'><span class='glyphicon glyphicon-save' style='color:#5a5a5a'></span>
+                    22</span>
+
+        </div>
+    </div><a href='download.php'>
+        <p class='btn btn-success btn-block' style='margin-top:10px;'>ทั้งหมด</p>
+    </a> </div>
+
+    </div>
